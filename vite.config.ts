062f6a1fd2b6bd5c287cc8e82030ec70bd9570ba
Pierre Jarnac <pjarnac@student.42lyon.fr>
2026-05-29 17:17:57 +0000

    feat: integrate tailwindcss into workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig(() => {
 	const proxyTarget = `http://keepgrouped-back-dev:8080`;
 
 	return {
-		plugins: [react()],
+		plugins: [react(), tailwindcss()],
 		server: {
 			allowedHosts: true,
 			proxy: {
