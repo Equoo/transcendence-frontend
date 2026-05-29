c765b0eac925d2be7a09e6b47bb03fa94ceb8b9c
Pierre Jarnac <pjarnac@student.42lyon.fr>
2026-05-29 17:14:30 +0000

    feat: add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), "");
-
+export default defineConfig(() => {
 	const proxyTarget = `http://keepgrouped-back-dev:8080`;
+
 	return {
 		plugins: [react()],
 		server: {
