7b52f8505b4e533698169f2d7e46f082a2c3e0d2
Pierre Jarnac <pjarnac@student.42lyon.fr>
2026-07-29 18:31:58 +0000

    fix: forgot to update react-router config after deletion of resource route

--- src/routes.ts
+++ src/routes.ts
@@ -21,5 +21,4 @@ export default [
 		"events/routes/registrations.route.tsx",
 	),
 	route("/events", "events/routes/events.route.tsx"),
-	route("/events_roles", "events/routes/event_roles.route.tsx"),
 ] satisfies RouteConfig;
