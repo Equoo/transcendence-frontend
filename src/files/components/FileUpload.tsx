90fe0752d685a6eb5daa074d6df147db8ee7b8f6
Pierre Jarnac <pjarnac@student.42lyon.fr>
2026-08-06 15:00:11 +0000

    feat: change file name automatically to the name of the uploaded file

---
+++ src/files/components/FileUpload.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState, type JSX } from "react";
+import Modal from "../../components/Modal";
+import type { clientAction as filesAction } from "../routes/files.route";
+import { useFetcher } from "react-router";
+import { Input } from "../../components/Input";
+import CheckButton from "../../components/CheckButton";
+
+export default function FileUpload({
+	onClose,
+}: {
+	onClose: () => void;
+}): JSX.Element {
+	const [name, setName] = useState("");
+	const filesFetcher = useFetcher<typeof filesAction>();
+
+	useEffect(() => {
+		if (filesFetcher.data) {
+			onClose();
+		}
+	}, [filesFetcher.data]);
+	return (
+		<Modal title="Upload a file" onClose={onClose}>
+			<filesFetcher.Form
+				action="/files"
+				method="POST"
+				encType="multipart/form-data"
+				className="flex flex-col items-center w-4/5 gap-5 mb-4"
+			>
+				<Input
+					name="Name"
+					required
+					value={name}
+					onChange={(ev) => {
+						setName(ev.target.value);
+					}}
+				/>
+				<Input
+					name="File"
+					type="file"
+					required
+					onChange={(ev) => {
+						setName(
+							ev.target.value.substring(
+								ev.target.value.lastIndexOf("\\") + 1,
+							),
+						);
+					}}
+				/>
+				<CheckButton
+					type="submit"
+					active
+					pending={filesFetcher.state !== "idle"}
+				>
+					Upload
+				</CheckButton>
+			</filesFetcher.Form>
+		</Modal>
+	);
+}
--- src/routes/knowledge.tsx
+++ src/routes/knowledge.tsx
@@ -1,14 +1,11 @@
-import { useEffect, useState, type JSX } from "react";
+import { useState, type JSX } from "react";
 import { fetchFiles, type AppFile } from "../files/api/files.api";
 import type { Route } from "./+types/knowledge";
 import FileList from "../files/components/FileList";
 import Promisable from "../events/components/Promisable";
 import CheckButton from "../components/CheckButton";
 import { PiUploadSimple } from "react-icons/pi";
-import Modal from "../components/Modal";
-import { Input } from "../components/Input";
-import type { clientAction as filesAction } from "../files/routes/files.route";
-import { useFetcher } from "react-router";
+import FileUpload from "../files/components/FileUpload";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type, @typescript-eslint/explicit-module-boundary-types
 export function clientLoader(): { files: Promise<AppFile[]> } {
@@ -19,14 +16,7 @@ export default function Knowledge({
 	loaderData,
 }: Route.ComponentProps): JSX.Element {
 	const [showUpload, setShowUpload] = useState(false);
-	const filesFetcher = useFetcher<typeof filesAction>();
 
-	useEffect(() => {
-		if (filesFetcher.data) {
-			// eslint-disable-next-line @eslint-react/set-state-in-effect
-			setShowUpload(false);
-		}
-	}, [filesFetcher.data]);
 	return (
 		<main className="w-7/10 justify-center flex flex-col gap-1">
 			<CheckButton
@@ -41,29 +31,11 @@ export default function Knowledge({
 				Upload
 			</CheckButton>
 			{showUpload && (
-				<Modal
-					title="Upload a file"
+				<FileUpload
 					onClose={() => {
 						setShowUpload(false);
 					}}
-				>
-					<filesFetcher.Form
-						action="/files"
-						method="POST"
-						encType="multipart/form-data"
-						className="flex flex-col items-center w-4/5 gap-5 mb-4"
-					>
-						<Input name="Name" required />
-						<Input name="File" type="file" required />
-						<CheckButton
-							type="submit"
-							active
-							pending={filesFetcher.state !== "idle"}
-						>
-							Upload
-						</CheckButton>
-					</filesFetcher.Form>
-				</Modal>
+				/>
 			)}
 			<Promisable data={loaderData.files}>
 				{(files) => <FileList files={files}></FileList>}
